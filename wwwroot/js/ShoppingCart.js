f7326347c80275f74b7a559345597fc0f0cef9f8
lernenCode <luizvinicius.emprego@hotmail.com>
2023-06-26 23:21:07 -0300

    refatorar code

--- wwwroot/js/ShoppingCart.js
+++ wwwroot/js/ShoppingCart.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Selecionar os botões de diminuir e aumentar a quantidade
     var decreaseQuantityButtons = document.querySelectorAll('.decrease-quantity');
     var increaseQuantityButtons = document.querySelectorAll('.increase-quantity');
+
+    // Selecionar os elementos que exibem os preços totais
     var totalPrices = document.querySelectorAll('[data-price-element]');
+
+    // Selecionar os inputs de quantidade
     var quantityInputs = document.querySelectorAll('.quantity');
 
+    // Função para diminuir a quantidade
     decreaseQuantityButtons.forEach(function (button, index) {
         button.addEventListener('click', function () {
             var quantityInput = this.closest('.modal').querySelector('.quantity');
@@ -16,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    // Função para aumentar a quantidade
     increaseQuantityButtons.forEach(function (button, index) {
         button.addEventListener('click', function () {
             var quantityInput = this.closest('.modal').querySelector('.quantity');
@@ -26,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
+    // Função para atualizar o preço total
     function updateTotalPrice(index) {
         var quantityInput = quantityInputs[index];
         var modal = quantityInput.closest('.modal');
@@ -43,14 +51,17 @@ document.addEventListener('DOMContentLoaded', function () {
         totalPrices[index].textContent = newTotal.toFixed(2);
     }
 
+    // Selecionar os checkboxes de produtos adicionais
     var additionalProductCheckboxes = document.querySelectorAll('.additional-product-checkbox');
 
+    // Função para lidar com a alteração dos checkboxes
     additionalProductCheckboxes.forEach(function (checkbox) {
         checkbox.addEventListener('change', function () {
             updateTotalPrices();
         });
     });
 
+    // Função para atualizar todos os preços totais
     function updateTotalPrices() {
         totalPrices.forEach(function (price, index) {
             updateTotalPrice(index);
